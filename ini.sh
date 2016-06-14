d6a6c3043b4d5379b461328e821368239806a566
shehantissera <shehan@duosoftware.com>
2016-06-14 16:04:14 +0530

    updated init

--- ini.sh
+++ ini.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-git clone https://github.com/DuoSoftware/Smoothflow.io-appserver-files.git
-mv Smoothflow.io-appserver-files/* /var/www/html/
+if [ -d "Smoothflow.io-appserver-files" ]; then 
+	cd Smoothflow.io-appserver-files
+	git pull
+	cd ../
+else
+	git clone https://github.com/DuoSoftware/Smoothflow.io-appserver-files.git
+fi
+
+#copying files
+cp -rf Smoothflow.io-appserver-files/* /var/www/html/
 
 #set permisions to other scripts
 chmod u+x /var/www/html/prepare/./prepare.sh
