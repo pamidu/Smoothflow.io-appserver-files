cd7bae239b5052b72bf2d7f1950b120b0a9a48cc
shehantissera <shehan@duosoftware.com>
2016-07-07 16:55:27 +0530

    changed configs

--- include/config_bkp.php
+++ include/config_bkp.php
@@ -1,7 +1,7 @@
 <?php
-$mainDomain="192.168.2.12";
-$authURI="http://192.168.2.12:3048/";
-$objURI="http://192.168.2.12:3000/";
+$mainDomain="dev.smoothflow.io";
+$authURI="http://auth.smoothflow.io:3048/";
+$objURI="http://obj.smoothflow.io:3000/";
 $fullhost=strtolower($_SERVER['HTTP_HOST']);
 define("ROOT_PATH", $_SERVER['DOCUMENT_ROOT']);
 define("MEDIA_PATH", "/var/media");
@@ -9,10 +9,10 @@ define("APPICON_PATH", "/var/www/html/devportal/appicons");
         //define("BASE_PATH", "/var/www/html/medialib");
         //define("STORAGE_PATH", BASE_PATH . "/media");
         //define("THUMB_PATH", BASE_PATH . "/thumbnails");
-define("SVC_OS_URL", "http://192.168.2.12:3000");
-define("SVC_OS_BULK_URL", "http://192.168.2.12:3001/transfer");
-define("SVC_AUTH_URL", "http://192.168.2.12:3048");
-define("SVC_CEB_URL", "http://192.168.2.12:3500");
-define("SVC_SMOOTHFLOW_URL", "http://192.168.2.12");
+define("SVC_OS_URL", "http://obj.smoothflow.io:3000");
+define("SVC_OS_BULK_URL", "http://obj.smoothflow.io:3001/transfer");
+define("SVC_AUTH_URL", "http://auth.smoothflow.io:3048");
+define("SVC_CEB_URL", "http://ceb.smoothflow.io:3500");
+define("SVC_SMOOTHFLOW_URL", "http://smoothflow.io");
 define ("STRIPE_KEY","sk_test_ujsydm6IgtDJ49L74x7sj4x7");
 ?>
