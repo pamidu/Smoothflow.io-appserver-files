751b556e4ede4bd6744b774e8641b82712926a60
shehantissera <shehan@duosoftware.com>
2016-08-01 12:20:11 +0530

    changed the comments
    removed unwanted code

--- index.php
+++ index.php
@@ -6,42 +6,29 @@ require_once ("include/session.php");
 $fullHost = strtolower($_SERVER['HTTP_HOST']);
 
 switch ($fullHost) {
-    case $mainDomain: // digin.io
+    case $mainDomain: // smoothflow.io
         if(!isset($_COOKIE["securityToken"])){
-			//echo 'chamila4'; exit();
-            //include ("index1.php");
 			header("location:http://www.smoothflow.io");
 		}
         else{
-			//echo 'chamila5'; exit();
             getURI();
 		}
         break;
-    case "www." . $mainDomain: // www.digin.io
+    case "www." . $mainDomain: // www.smoothflow.io
         if(!isset($_COOKIE["securityToken"])){
-			//echo 'chamila6'; exit();
-            include ("index1.php");
-			
-			}
-        else{
-			//echo 'chamila7'; exit();
-		getURI();
+            include ("index1.php");	
+		}else{
+		    getURI();
 		}
         break;
     default:
     if(!isset($_COOKIE["securityToken"])) {
-		//echo 'chamila8'; exit();
         if($mainDomain != $_SERVER['HTTP_HOST']){ 
-			//echo 'chamila9'; exit();
             header("Location: http://". $mainDomain . "/login.php?r=http://" . $_SERVER['HTTP_HOST'] . '/s.php'); 
 		}
         exit();
     }
-	
-	//echo 'chamila10'; exit();
-	//include ("index1.php");
 	getURI();
-	
     break;
 }
 ?>
