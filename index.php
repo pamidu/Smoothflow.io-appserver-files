469f84891e47461535b6d6113d21f38e113fe927
shehantissera <shehan@duosoftware.com>
2016-08-04 18:18:58 +0530

    WIP

--- index.php
+++ index.php
@@ -8,13 +8,13 @@ $fullHost = strtolower($_SERVER['HTTP_HOST']);
 switch ($fullHost) {
     case $mainDomain: // smoothflow.io
         if(!isset($_COOKIE["securityToken"])){
-			header("location:http://qa.smoothflow.io");
+			header("location:http://www.smoothflow.io");
 		}
         else{
             getURI();
 		}
         break;
-    case "qa." . $mainDomain: // qa.smoothflow.io
+    case "www." . $mainDomain: // www.smoothflow.io
         if(!isset($_COOKIE["securityToken"])){
             include ("index1.php");	
 		}else{
@@ -24,7 +24,7 @@ switch ($fullHost) {
     default:
     if(!isset($_COOKIE["securityToken"])) {
         if($mainDomain != $_SERVER['HTTP_HOST']){ 
-            header("Location: http://qa.". $mainDomain . "/login.php?r=http://" . $_SERVER['HTTP_HOST'] . '/s.php'); 
+            header("Location: http://". $mainDomain . "/login.php?r=http://" . $_SERVER['HTTP_HOST'] . '/s.php'); 
 		}
         exit();
     }
