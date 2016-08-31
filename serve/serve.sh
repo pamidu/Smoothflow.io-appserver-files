1f476e5339f34c06835e04368ad498c3fa38dca2
shehantissera <shehan@duosoftware.com>
2016-08-31 11:05:31 +0530

    remove all files with the size of zero

--- serve/serve.sh
+++ serve/serve.sh
@@ -16,6 +16,9 @@ echo "Starting ProcessEngine service"
 
 cd /var/www/html/engine/
 
+#remove all files which are in the size of 0 bytes
+find . -size 0c -delete
+
 #give permission to all files to execute
 chmod 777 *
 ./processengine
